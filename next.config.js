bcd5a43ff891c78ef1108aa5870411140ede66ac
Huy <kingbazoka@gmail.com>
2019-01-25 23:36:43 -0800

    Update build config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-const webpack = require('webpack');
-const withTypescript = require('@zeit/next-typescript');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-module.exports = withTypescript({
-    webpack(config, options) {
-        if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
-        return config;
-    }
-});
+const { PHASE_PRODUCTION_SERVER } =
+      process.env.NODE_ENV === 'development'
+      ? {}
+      : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
 
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        return {};
+    }
+    const withTypescript = require('@zeit/next-typescript');
+    const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+    return withTypescript({
+        webpack(config, options) {
+            if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+            return config;
+        }
+    });
+}
