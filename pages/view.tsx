af51fb8aed50a364d8d312eb9de3428a86f75a01
Huy <kingbazoka@gmail.com>
2019-01-27 19:38:54 -0800

    Add comment in posts

--- pages/view.tsx
+++ pages/view.tsx
@@ -1,20 +1,46 @@
 import { Component, ReactNode } from 'react';
 import 'isomorphic-unfetch';
 
+type Comment = {
+    content: string;
+    username: string;
+    avatar: string;
+};
+
+type CommentProps = {
+    key: number;
+    comment: Comment;
+};
+
+const CommentItem = (props: CommentProps) => {
+    return <li className="p-2 border-b border-grey-light text-md">
+        <div className="font-semibold">{props.comment.username}</div>
+        <div className="my-2" dangerouslySetInnerHTML={{ __html: props.comment.content }}></div>
+    </li>;
+};
+
 type PropsType = {
     page: number;
     title: string;
     content: string;
+    comments: Comment[];
 };
 
 export default class View extends Component<PropsType> {
     static async getInitialProps(params: any) {
         const data = await fetch(`https://p.voz.vn/posts/${params.query.id}`);
         const json = await data.json();
+        const comments = await fetch(`https://p.voz.vn/posts/${params.query.id}/comments`);
+        const comments_json = await comments.json();
         return {
             page: parseInt(params.query.page) || 1,
             title: json.title,
-            content: json.content
+            content: json.content,
+            comments: comments_json.results.map((c: any) => ({
+                content: c.content,
+                username: c.user_meta.display_name,
+                avatar: c.user_meta.photo_url
+            }))
         };
     }
 
@@ -25,6 +51,9 @@ export default class View extends Component<PropsType> {
                 <a href={`/index?page=${(this.props.page)}`} className="p-2 bg-transparent hover:bg-blue hover:text-white text-blue-dark font-semibold border border-blue rounded no-underline">← back</a>
                 <h2 className="my-2">{this.props.title}</h2>
                 <div className="my-2" dangerouslySetInnerHTML={{ __html: this.props.content }}></div>
+                <ul className="p-5 my-2 bg-grey-lightest list-reset">
+                { this.props.comments.map((c: Comment, id: number) => <CommentItem key={id} comment={c}/>) }
+                </ul>
                 <a href={`/index?page=${(this.props.page)}`} className="p-2 bg-transparent hover:bg-blue hover:text-white text-blue-dark font-semibold border border-blue rounded no-underline">← back</a>
             </div>
         )
