7290188bd7c5104a5de70631fd8b134ffc4282a0
Huy <kingbazoka@gmail.com>
2019-01-28 11:57:49 -0800

    Add image and URL for og meta tags

--- pages/view.tsx
+++ pages/view.tsx
@@ -1,6 +1,8 @@
-import { Component, ReactNode } from 'react';
+ import { Component, ReactNode } from 'react';
 import Head from 'next/head';
 import 'isomorphic-unfetch';
+
+type Maybe<T> = T | null;
 
 type Comment = {
     content: string;
@@ -21,6 +23,7 @@ const CommentItem = (props: CommentProps) => {
 };
 
 type PropsType = {
+    id: number;
     page: number;
     title: string;
     content: string;
@@ -34,6 +37,7 @@ export default class View extends Component<PropsType> {
         const comments = await fetch(`https://p.voz.vn/posts/${params.query.id}/comments`);
         const comments_json = await comments.json();
         return {
+            id: json.id,
             page: parseInt(params.query.page) || 1,
             title: json.title,
             content: json.content,
@@ -45,13 +49,23 @@ export default class View extends Component<PropsType> {
         };
     }
 
+    getFirstImage(input: string): Maybe<string> {
+        const match: Maybe<RegExpMatchArray> = input.match(/<img.*?src=\"(.*?)\"/);
+        if (match && match.length > 1) {
+            return match[1];
+        }
+        return null;
+    }
+
     render(): ReactNode {
         return (
             <div className="container max-w-md sm mx-auto p-4 font-sans text-md text-grey-darkest leading-normal">
                 <Head>
-                    <title>{this.props.title}</title>
-                    <meta property="og:title" content={this.props.title} />
-                    <meta property="og:type" content="article" />
+                <title>{this.props.title}</title>
+                <meta property="og:title" content={this.props.title} />
+                <meta property="og:type" content="article" />
+                <meta property="og:url" content={`https://voz-news.now.sh/view?id=${this.props.id}&page=${this.props.page}`} />
+                <meta property="og:image" content={`${this.getFirstImage(this.props.content)}`} />
                 </Head>
                 <style jsx global>{`blockquote { background: #fafafa; padding: 10px; border-left: 2px solid #eee; }`}</style>
                 <a href={`/index?page=${(this.props.page)}`} className="p-2 bg-transparent hover:bg-blue hover:text-white text-blue-dark font-semibold border border-blue rounded no-underline">← back</a>
